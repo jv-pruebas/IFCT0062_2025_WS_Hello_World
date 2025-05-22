eaf06a990722aa31e799e21f01f1bd7f19182c50
jv <jv_to@hotmail.com>
2025-05-22 19:11:08 +0200

    modificación en el controller

--- src/main/java/com/christianro/IFCT0062_2025_WS_Hello_World/controller/SaludadorController.java
+++ src/main/java/com/christianro/IFCT0062_2025_WS_Hello_World/controller/SaludadorController.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class SaludadorController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "HelloHolita, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/saludo")
